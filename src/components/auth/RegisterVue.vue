c959411edb3c5afcc50a581643b24406af3b09b7
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-12 12:11:43 +0100

    register text

--- src/components/auth/RegisterVue.vue
+++ src/components/auth/RegisterVue.vue
@@ -3,6 +3,16 @@
     <div class="my-5 text-center offset-2 col-8">
       <img src="@/assets/welcome.svg" class="img-fluid" />
     </div>
+    <div class="text-center">
+      <h2>Jetzt registrieren</h2>
+      <p>
+        oder
+        <a class="text-vue2" role="button"
+          >Melden Sie sich mit Ihrem Konto an</a
+        >
+        <!-- role="button" bedeutet, dass sich der Cursor ändert! -->
+      </p>
+    </div>
   </div>
 </template>
 
