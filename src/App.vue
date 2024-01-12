55e33544fe3c0585f6ba80a866be670948c5ee8b
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-12 11:19:38 +0100

    shop create

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+@import "~bootstrap/dist/css/bootstrap.min.css";
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+@import "~animate.css/animate.min.css";
+
+.bg-vue {
+}
+</style>
