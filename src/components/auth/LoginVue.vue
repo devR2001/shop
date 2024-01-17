8147b64176def2136fde1c750fae9e46dc17e07f
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-17 17:57:18 +0100

    Funktionen in Login.vue kopieren

--- src/components/auth/LoginVue.vue
+++ src/components/auth/LoginVue.vue
@@ -13,7 +13,9 @@
         <!-- role="button" bedeutet, dass sich der Cursor ändert! -->
       </p>
     </div>
-    <Form></Form>
+    <div class="alert alert-danger col-md-8 offset-2">
+      {{ errorDisplayText }}
+    </div>
     <Form @submit="submitData" :validation-schema="schema" v-slot="{ errors }">
       <div class="form-row">
         <div class="form-group col-md-8 offset-2">
@@ -48,7 +50,10 @@
       <div class="form-row mt-3">
         <div class="form-group col-md-8 offset-2">
           <div class="d-grid">
-            <button class="btn bg-vue">Einloggen</button>
+            <button class="btn bg-vue">
+              <span v-if="!isLoading">Einloggen</span>
+              <span v-else class="spinner-border spinner-border-sm"></span>
+            </button>
           </div>
         </div>
       </div>
@@ -59,6 +64,8 @@
 <script>
 import { Form, Field } from "vee-validate";
 import * as yup from "yup";
+import axios from "axios";
+import { FIREBASE_API_KEY } from "../../config/firebase";
 
 export default {
   name: "LoginVue",
@@ -88,11 +95,46 @@ export default {
     });
     return {
       schema,
+      error: "",
+      isLoading: false,
     };
+  },
+  computed: {
+    errorDisplayText() {
+      if (this.error) {
+        if (this.error.includes("EMAIL_EXISTS")) {
+          return "Die E-Mail Adresse existiert bereits.";
+        }
+        return "Es ist ein unbekannter Fehler aufgetreten. Bitte versuchen Sue es noch einmal.";
+      }
+      return "";
+    },
   },
   methods: {
     submitData(values) {
-      console.log(values);
+      this.isLoading = true;
+      this.error = "";
+      // console.log(values);
+      const signupDO = {
+        email: values.email,
+        password: values.password,
+        returnSecureToken: true,
+      };
+      axios
+        .post(
+          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`,
+          signupDO
+        )
+        .then((response) => {
+          console.log(response);
+          this.isLoading = false;
+          this.changeComponent("login");
+        })
+        .catch((error) => {
+          // console.log({ error });
+          this.error = error.response.data.error.message;
+          this.isLoading = false;
+        });
     },
     changeComponent(componentName) {
       this.$emit("change-component", { componentName });
