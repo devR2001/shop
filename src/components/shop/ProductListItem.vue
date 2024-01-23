820a9979958e972c8223b3e98b028bf50d86c30c
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-23 17:34:20 +0100

    ProductListItem-Component erstellen

---
+++ src/components/shop/ProductListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <img src="https://picsum.photos/200" />
+    <div class="card-body">
+      <h5 class="card-title">Titel</h5>
+      <p class="card-text">Beschreibung</p>
+      <div class="text-center mt-3">
+        <div class="lead">
+          <strong class="text-vue">15 €</strong>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductListItem",
+};
+</script>
+
+<style scoped></style>
--- src/pages/ShopPage.vue
+++ src/pages/ShopPage.vue
@@ -1,17 +1,23 @@
 <template>
   <TheShopLayout>
     <template #default>
-      <h2>Hallo</h2>
+      <div class="row">
+        <div class="col-3 m-3" v-for="i in 3" :key="i">
+          <ProductListItem />
+        </div>
+      </div>
     </template>
   </TheShopLayout>
 </template>
 
 <script>
 import TheShopLayout from "@/layouts/TheShopLayout.vue";
+import ProductListItem from "@/components/shop/ProductListItem.vue";
 export default {
   name: "ShopPage",
   components: {
     TheShopLayout,
+    ProductListItem,
   },
 };
 </script>
