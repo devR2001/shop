4f1cfbb45c6d9e389cda808af7af0b6884d570fa
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-24 17:51:27 +0100

    Das Warenkorb Modul

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from "vuex";
 import authModule from "./modules/auth";
 import shopModule from "./modules/shop";
+import cartModule from "./modules/cart";
 
 const store = createStore({
   modules: {
     auth: authModule,
     shop: shopModule,
+    cart: cartModule,
   },
 });
 
