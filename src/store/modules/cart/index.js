49e4b24aafd2fd69ef7c658890f2cc545268b15d
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-24 18:12:44 +0100

    Module noch weiter modularisieren und mutation types

--- src/store/modules/cart/index.js
+++ src/store/modules/cart/index.js
@@ -1,30 +1,7 @@
-const state = {
-  cartItems: [],
-};
-const mutations = {
-  addCartItem(state, payload) {
-    state.cartItems.push(payload);
-  },
-};
-// context = { commit, dispatch, state, rootState, getters, rootGetters }
-const actions = {
-  addItemToCart({ commit }, payload) {
-    commit("addCartItem", {
-      productId: payload.productId,
-    });
-  },
-};
-const getters = {
-  cartItems: (state) => state.cartItems,
-  //  (state, getters, rootState, rootGetters)
-  cartTotal: (_, getters, __, rootGetters) => {
-    const cartItems = getters.cartItems;
-    return cartItems.reduce((sum, cartItem) => {
-      const product = rootGetters.product(cartItem.productId);
-      return sum + product.price;
-    }, 0);
-  },
-};
+import state from "./state";
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
 
 const cartModule = {
   state,
